02a8dddb6478ee04b0aca87e48c9f772f3830def
NeilRow56 <neilrowland56@gmail.com>
2024-05-30 12:03:32 +0100

    Added Block Note editor

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,11 +1,14 @@
 'use client'
 
-import { useQuery } from 'convex/react'
+import { useMutation, useQuery } from 'convex/react'
 
 import { api } from '@/convex/_generated/api'
 import { Id } from '@/convex/_generated/dataModel'
 import { Toolbar } from '@/components/Toolbar'
 import { Cover } from '@/components/Cover'
+import { Skeleton } from '@/components/ui/skeleton'
+
+import { Editor } from '@/components/Editor'
 
 interface DocumentIdPageProps {
   params: {
@@ -18,8 +21,28 @@ const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
     documentId: params.documentId,
   })
 
+  const update = useMutation(api.documents.update)
+  const onChange = (content: string) => {
+    update({
+      id: params.documentId,
+      content,
+    })
+  }
+
   if (document === undefined) {
-    return <div>Loading...</div>
+    return (
+      <div>
+        <Cover.Skeleton />
+        <div className="mx-auto mt-10 md:max-w-3xl lg:max-w-4xl">
+          <div className="space-y-4 pl-8 pt-4">
+            <Skeleton className="h-14 w-[50%]" />
+            <Skeleton className="h-4 w-[80%]" />
+            <Skeleton className="h-4 w-[40%]" />
+            <Skeleton className="h-4 w-[60%]" />
+          </div>
+        </div>
+      </div>
+    )
   }
 
   if (document === null) {
@@ -31,6 +54,7 @@ const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
       <Cover url={document.coverImage} />
       <div className="md: mx-auto max-w-3xl lg:max-w-4xl">
         <Toolbar initialData={document} />
+        <Editor onChange={onChange} initialContent={document.content} />
       </div>
     </div>
   )
